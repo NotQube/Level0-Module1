a3d3c19a3f88f526c953ee8e907717bb9216085c
league <notqube>
2023-10-08 12:56:53 -0700

    hjjhs

--- _03_if_else/_2_remarkable/remarkable.py
+++ _03_if_else/_2_remarkable/remarkable.py
@@ -2,6 +2,16 @@ import turtle
 from tkinter import messagebox, simpledialog, Tk
 if __name__ == '__main__':
     window = Tk()
+    name = simpledialog.askstring(title='question', prompt="what is your name?")
+    if name == "henry":
+        messagebox.showinfo(title="answer",message="why r u you doing this HENRY")
+    if name == "kasra":
+        messagebox.showinfo(title="answer",message="why r u you doing this KASRA")
+    if name == "jackson":
+        messagebox.showinfo(title="answer",message="why r u you doing this JACKSON")
+    if name == "liam":
+        messagebox.showinfo(title="answer",message="why r u you doing this LIAM!")
+
 
 
 
--- _03_if_else/_4_circle_calculator/circle_calculator.py
+++ _03_if_else/_4_circle_calculator/circle_calculator.py
@@ -1,6 +1,10 @@
 # Write a Python program that asks the user for the radius of a circle.
+radius = simpledialog.askstring(title='question', prompt="what is the radius of the circle that you want to make?")
 # Next, ask the user if they would like to calculate the area or circumference of a circle.
+circle = simpledialog.askstring(title='question', prompt="do you want the area or the circumference of the circle")
 # If they choose area, display the area of the circle using the radius.
+if circle:
+    area
 # Otherwise, display the circumference of the circle using the radius.
 
 #Area = πr^2
